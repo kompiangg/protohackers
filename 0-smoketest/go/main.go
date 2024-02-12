515a8971abca42bd0e02306a5b65a3c404f2aee7
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2024-02-13 04:03:01 +0800

    go smoke test

---
+++ 0-smoketest/go/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	port := flag.Int("port", 9000, "specify the listened port")
+	address := flag.String("address", "0.0.0.0", "specify the listened address")
+	flag.Parse()
+
+	tcpAddress := fmt.Sprintf("%s:%d", *address, *port)
+	tcpListener, err := net.Listen("tcp", tcpAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	defer tcpListener.Close()
+
+	log.Printf("[Info] listened tcp in %s\n", tcpAddress)
+
+	for {
+		conn, err := tcpListener.Accept()
+		if err != nil {
+			log.Printf("[Error] error when accept the connections: %v", err)
+			continue
+		}
+
+		go func(c net.Conn) {
+			_, err := io.Copy(c, c)
+			if err != nil {
+				log.Printf("[Error] error when copy the data: %v", err)
+			}
+
+			c.Close()
+		}(conn)
+	}
+}
